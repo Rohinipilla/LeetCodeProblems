fff41d6147a8d3d559f128188896b1705910b691
Rohinipilla <138279046+Rohinipilla@users.noreply.github.com>
2024-01-14 21:00:20 +0530

    Create 283. Move Zeroes.cpp

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int i=0,j=0;
+        while(i<nums.size() and  j<nums.size()){
+            if(nums[i]!=0){
+                i++;
+            }
+            if(nums[j]==0){
+                j++;
+            }
+            if((i<nums.size() and j<nums.size()) and (i<j)){
+                swap(nums[i],nums[j]);
+            }
+            j++;
+           
+        }
+    }
+};
