bf58720f334b69ae919d40468bf921ac0f6a9a2e
Rohinipilla <138279046+Rohinipilla@users.noreply.github.com>
2024-01-29 23:06:32 +0530

    Create 350. Intersection of Two Arrays II.cpp

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> intersect;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        int x=nums1.size();
+        int y=nums2.size();
+
+        int i=0,j=0;
+
+        while(i<x && j<y){
+            
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                intersect.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        return intersect;
+    }
+};
